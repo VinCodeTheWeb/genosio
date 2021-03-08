0bbc7f815d84be476f1f42180afa57ab65b82ec6
Vincenzo PELLEGRINI <vincenzo33.pellegrini@gmail.com>
2021-03-08 23:59:31 +0100

    removing types in tsconfig.json [backend]

--- client/src/components/Header/header.test.ts
+++ client/src/components/Header/header.test.ts
@@ -1,8 +1,3 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import { Header } from './header.component';
-
-it('should render', () => {
-    const { getByTestId } = render(<Header />);
-    expect(getByTestId('header')).toHaveTextContent(0);
-});
