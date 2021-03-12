9ba98e7873a309cc4b27b43ffc619b5634a44bb7
Vincenzo PELLEGRINI <vincenzo33.pellegrini@gmail.com>
2021-03-12 23:44:49 +0100

    adding theme.config file a its own theme folder for better structure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
@@ -8,7 +8,7 @@ import './i18n/config';
 import './styles/_base.css';
 
 // App Theme
-import { THEME } from 'styles/theme.styles';
+import { THEME } from 'theme/theme.config';
 
 //  CssBaseLine For Global Reset
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -19,9 +19,9 @@ import { App } from 'components/App/app.component';
 ReactDOM.render(
     <ThemeProvider theme={createMuiTheme(THEME)}>
         <CssBaseline />
-        <Suspense fallback={<div>Loading...</div>}>
+        <React.Suspense fallback={<div>Loading...</div>}>
             <App />
-        </Suspense>
+        </React.Suspense>
     </ThemeProvider>,
     document.getElementById('root'),
 );
