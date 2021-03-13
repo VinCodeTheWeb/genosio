700152b0c16845ec02b8fcc2e36729f335fc3ad9
Vincenzo PELLEGRINI <vincenzo33.pellegrini@gmail.com>
2021-03-13 23:43:04 +0100

    removing uneeded border in menu

--- client/src/components/Menu/menu.styles.ts
+++ client/src/components/Menu/menu.styles.ts
@@ -2,9 +2,7 @@ import { createStyles, makeStyles } from '@material-ui/core';
 
 const styles = () =>
     createStyles({
-        menu: {
-            border: '1px solid red',
-        },
+        menu: {},
     });
 
 export const useStyles = makeStyles(styles);
